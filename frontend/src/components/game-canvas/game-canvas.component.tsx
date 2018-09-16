c9f14117999b05acf4f14ac81b632589b36627be
Aulden <aulden.harris@gmail.com>
2018-09-16 18:22:31 -0400

    Added requisite event to front end

--- frontend/src/components/game-canvas/game-canvas.component.tsx
+++ frontend/src/components/game-canvas/game-canvas.component.tsx
@@ -67,6 +67,7 @@ export class GameCanvasComponent extends React.Component<any, any> {
 
 
   public componentDidMount() {
+    this.socket.emit('new player' );
 
     this.socket.on('show art', (players: any[]) => {
       console.log(players);
